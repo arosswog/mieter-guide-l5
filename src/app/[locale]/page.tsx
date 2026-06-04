8b900d5eb66d8b88b970c45178ddcf76fbc0c7a4
Alexander Rosswog <arosswog@MacBookPro.rosswog.local>
2026-06-04 11:20:37 +0200

    Fix: Add missing Link import

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { useLocale, useTranslations } from 'next-intl';
 import { MdWifi, MdLogin, MdHome, MdContactPhone, MdDirectionsTransit, MdShoppingCart, MdLocationOn, MdShoppingBag } from 'react-icons/md';
 
